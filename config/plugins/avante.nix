ecd0276d9ae1fb210a330adbe3924ac82e78ed0c
caverav <camilo@fvv.cl>
2025-01-30 14:14:42 -0300

    feat(avante.nix): add configuration settings for avante plugin to enhance functionality and customization options

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -2,6 +2,18 @@
   plugins = {
     avante = {
       enable = true;
+      settings = {
+        provider = "copilot";
+        hints.enabled = true;
+        windows = {
+          wrap = true;
+          width = 30;
+          sidebar_header = {
+            align = "center";
+            rounded = true;
+          };
+        };
+      };
     };
   };
 }
